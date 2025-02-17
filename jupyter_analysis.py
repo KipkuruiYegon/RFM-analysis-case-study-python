5d71207ff5365f7ff28d9950557a1b379e48bf0f
KipkuruiYegon <kevinkipkurui1@gmail.com>
2025-02-17 22:24:35 +0300

    date format change

--- jupyter_analysis.py
+++ jupyter_analysis.py
@@ -7,3 +7,25 @@ pio.templates.default = "plotly_white"
 data = pd.read_csv("rfm_data.csv")
 print(data.head())
 
+from datetime import datetime
+
+# Convert 'PurchaseDate' to datetime
+data['PurchaseDate'] = pd.to_datetime(data['PurchaseDate'], format='%m/%d/%Y')
+
+
+# Calculate Recency
+data['PurchaseDate'] = pd.to_datetime(data['PurchaseDate'], format='%Y-%m-%d')
+
+
+# Calculate Frequency
+frequency_data = data.groupby('CustomerID')['OrderID'].count().reset_index()
+frequency_data.rename(columns={'OrderID': 'Frequency'}, inplace=True)
+data = data.merge(frequency_data, on='CustomerID', how='left')
+
+# Calculate Monetary Value
+monetary_data = data.groupby('CustomerID')['TransactionAmount'].sum().reset_index()
+monetary_data.rename(columns={'TransactionAmount': 'MonetaryValue'}, inplace=True)
+data = data.merge(monetary_data, on='CustomerID', how='left')
+
+
+
